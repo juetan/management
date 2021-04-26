d19b173b08bf7f9e009c0f5e937f39497acf25e3
juetan <810335188@qq.com>
2021-04-26 16:42:17 +0800

    修复element-ui功能色在开发环境和生产环境下不一致的问题

--- src/config/theme-colors.js
+++ src/config/theme-colors.js
@@ -1,16 +1,17 @@
 const forElementUI = require("webpack-theme-color-replacer/forElementUI");
 
 // 两个主题色 这里使用COMMONJS语法，因为在vue.config.js不能使用es module语法
+// 颜色的16进制能简写尽量简写，例如#3c9不要写成#33cc99，否则会造成开发环境和生产环境下的样式不一致的问题
 module.exports = {
   green: [
     // element-ui主题色
     ...forElementUI.getElementUISeries("#10c599"),
     // element-ui功能色-成功色
-    ...forElementUI.getElementUISeries("#33cc99"),
+    ...forElementUI.getElementUISeries("#3c9"),
     // element-ui功能色-警报色
-    ...forElementUI.getElementUISeries("#ff9900"),
+    ...forElementUI.getElementUISeries("#f90"),
     // element-ui功能色-危险色
-    ...forElementUI.getElementUISeries("#ff6666"),
+    ...forElementUI.getElementUISeries("#f66"),
     // element-ui功能色-信息色
     ...forElementUI.getElementUISeries("#959595"),
     // 侧边栏背景色
